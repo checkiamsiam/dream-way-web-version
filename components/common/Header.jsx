6c21770493e36ea4f86ed8070e263a731c8010f5
checkiamsiam <issiam02415@gmail.com>
2023-01-13 11:16:37 +0600

    notice page done

--- components/common/Header.jsx
+++ components/common/Header.jsx
@@ -27,6 +27,7 @@ const Header = () => {
         route.pathname === "/login" ||
         route.pathname === "/flats" ||
         route.pathname === "/register" ||
+        route.pathname === "/notice-board" ||
         route.pathname === "/contact" ||
         route.pathname === "/lands"
           ? "stricky-fixed "
