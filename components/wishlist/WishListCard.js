894bc21b0cbd398fb91cde7d28fb37e340c4055a
checkiamsiam <issiam02415@gmail.com>
2023-01-14 13:12:28 +0600

    reassign

--- components/wishlist/WishListCard.js
+++ components/wishlist/WishListCard.js
@@ -2,18 +2,12 @@ import Image from "next/image";
 import Link from "next/link";
 import notificationBell from "../../public/assets/images/about/1.jpg";
 import { ImCart } from "react-icons/im";
-import { convertStringToArray } from "../common/utilityFunctions";
-function WishListCard({ item }) {
+function WishListCard() {
   return (
     <div className="feat_property list favorite_page">
       <div className="thumb">
-        <Image
-          width={100}
-          height={100}
-          className="img-whp cover w-auto h-auto"
-          src={notificationBell}
-          alt="fp1.jpg"
-        />
+        <Image className="img-whp cover w-auto h-auto" src={notificationBell} alt="fp1.jpg" />
+      
       </div>
       {/* End .thumb */}
 
@@ -21,13 +15,13 @@ function WishListCard({ item }) {
         <div className="tc_content">
           <h4>
             {" "}
-            <Link href="">{item?.title}</Link>
+            <Link href="">Dhanmondi Propertry</Link>
           </h4>
           <p>
-            <span className="flaticon-placeholder"></span> {item?.address}
+            <span className="flaticon-placeholder"></span> 1421 San Pedro St, Los Angeles, CA 900015
           </p>
           <a className="fp_price text-thm" href="#">
-            $ {item?.price || item.regular_price} BDT
+            100000 BDT
           </a>
         </div>
       </div>
@@ -41,7 +35,7 @@ function WishListCard({ item }) {
         </li>
         <li className="list-inline-item" data-toggle="tooltip" data-placement="top" title="Place Order">
           <a href="#">
-            <ImCart style={{ color: "#FF5A5F" }} />
+            <ImCart style={{color: "#FF5A5F"}}/>
           </a>
         </li>
       </ul>
