d960cecb49fb78b355fdf6bf6d8997063a0ad70e
Tareq Hassan <tareqhassan2014.com>
2023-01-14 12:36:54 +0600

    update env

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["dreamwayapi.sajidurapp.xyz"],
-  },
+    reactStrictMode: true,
+    images: {
+        domains: ['dreamwayapi.sajidurapp.xyz'],
+    },
 };
 
 module.exports = nextConfig;
