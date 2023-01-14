94f08bc6177a3d2a864c8b6f592b8807508b3cb6
Tareq Hassan <tareqhassan2014.com>
2023-01-14 13:29:28 +0600

    update wish list card

--- components/wishlist/WishListCard.js
+++ components/wishlist/WishListCard.js
@@ -1,47 +1,72 @@
-import Image from "next/image";
-import Link from "next/link";
-import notificationBell from "../../public/assets/images/about/1.jpg";
-import { ImCart } from "react-icons/im";
-function WishListCard() {
-  return (
-    <div className="feat_property list favorite_page">
-      <div className="thumb">
-        <Image className="img-whp cover w-auto h-auto" src={notificationBell} alt="fp1.jpg" />
-      
-      </div>
-      {/* End .thumb */}
+import Image from 'next/image';
+import Link from 'next/link';
+import { ImCart } from 'react-icons/im';
+import { convertStringToArray } from '../common/utilityFunctions';
 
-      <div className="details">
-        <div className="tc_content">
-          <h4>
-            {" "}
-            <Link href="">Dhanmondi Propertry</Link>
-          </h4>
-          <p>
-            <span className="flaticon-placeholder"></span> 1421 San Pedro St, Los Angeles, CA 900015
-          </p>
-          <a className="fp_price text-thm" href="#">
-            100000 BDT
-          </a>
+function WishListCard({ item }) {
+    console.log(item?.property);
+
+    return (
+        <div className="feat_property list favorite_page">
+            <div className="thumb">
+                <Image
+                    className="img-whp cover w-auto h-auto"
+                    src={`https://dreamwayapi.sajidurapp.xyz/${
+                        convertStringToArray(item?.property?.img_url)[0]
+                    }`}
+                    alt="fp1.jpg"
+                    width={200}
+                    height={200}
+                />
+            </div>
+            {/* End .thumb */}
+
+            <div className="details">
+                <div className="tc_content">
+                    <h4>
+                        {' '}
+                        <Link href="">{item?.property?.title}</Link>
+                    </h4>
+                    <p>
+                        <span className="flaticon-placeholder"></span>{' '}
+                        {item?.property?.address}
+                    </p>
+                    <a className="fp_price text-thm" href="#">
+                        {item?.property?.price || item?.property?.regular_price}{' '}
+                        BDT
+                    </a>
+                </div>
+            </div>
+            {/* End details */}
+
+            <ul className="view_edit_delete_list mb0 mt35">
+                <li
+                    className="list-inline-item"
+                    data-toggle="tooltip"
+                    data-placement="top"
+                    title="Remove"
+                    onClick={() => {
+                        console.log('remove');
+                    }}
+                >
+                    <a href="#">
+                        <span className="flaticon-garbage"></span>
+                    </a>
+                </li>
+                <li
+                    className="list-inline-item"
+                    data-toggle="tooltip"
+                    data-placement="top"
+                    title="Place Order"
+                >
+                    <a href="#">
+                        <ImCart style={{ color: '#FF5A5F' }} />
+                    </a>
+                </li>
+            </ul>
+            {/* view_edit_delete_list */}
         </div>
-      </div>
-      {/* End details */}
-
-      <ul className="view_edit_delete_list mb0 mt35">
-        <li className="list-inline-item" data-toggle="tooltip" data-placement="top" title="Remove">
-          <a href="#">
-            <span className="flaticon-garbage"></span>
-          </a>
-        </li>
-        <li className="list-inline-item" data-toggle="tooltip" data-placement="top" title="Place Order">
-          <a href="#">
-            <ImCart style={{color: "#FF5A5F"}}/>
-          </a>
-        </li>
-      </ul>
-      {/* view_edit_delete_list */}
-    </div>
-  );
+    );
 }
 
 export default WishListCard;
