dff954811ff7dc17678cadf90b0ad7467d1ccda5
checkiamsiam <issiam02415@gmail.com>
2023-01-14 14:59:23 +0600

    my property api connect

--- components/myProperty/RequestedPropertyCard.js
+++ components/myProperty/RequestedPropertyCard.js
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
 import notificationBell from "../../public/assets/images/about/1.jpg";
-function RequestedPropertyCard({property}) {
+function RequestedPropertyCard({ property }) {
   console.log(property);
   return (
     <div className="feat_property list favorite_page">
       <div className="thumb">
-        <Image className="img-whp cover w-auto h-auto" src={notificationBell} alt="fp1.jpg" />
+        <Image
+          className="img-whp cover w-auto h-auto"
+          src={`https://dreamwayapi.sajidurapp.xyz/${convertStringToArray(property?.property?.img_url)[0]}`}
+          alt="fp1.jpg"
+        />
       </div>
       {/* End .thumb */}
 
@@ -14,13 +18,13 @@ function RequestedPropertyCard({property}) {
         <div className="tc_content">
           <h4>
             {" "}
-            <Link href="">Dhanmondi Propertry</Link>
+            <Link href="">{property?.property?.title}</Link>
           </h4>
           <p>
-            <span className="flaticon-placeholder"></span> 1421 San Pedro St, Los Angeles, CA 900015
+            <span className="flaticon-placeholder"></span> {property?.property?.address}
           </p>
           <a className="fp_price text-thm" href="#">
-            100000 BDT
+          {property?.property?.price || property?.property?.regular_price} BDT
           </a>
         </div>
       </div>
