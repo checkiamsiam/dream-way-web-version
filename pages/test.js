f83e573bfd769afe9052d1c209165c988d331c53
Tareq Hassan <tareqhassan2014.com>
2023-01-15 15:45:00 +0600

    update test page

--- pages/test.js
+++ pages/test.js
@@ -10,27 +10,7 @@ const Test = () => {
     });
 
     return (
-        <div id="body">
-            <div
-                id="title"
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    fontSize: '28px',
-                    fontWeight: 'bold',
-                    marginTop: '10px',
-                    marginBottom: '10px',
-                    color: 'blue',
-                }}
-            >
-                Player stream RTSP
-            </div>
-            <div
-                id="video-canvas"
-                style={{ height: '480px', width: '640px' }}
-            ></div>
-        </div>
+        <div id="video-canvas" style={{ height: '480px', width: '640px' }} />
     );
 };
 
