7b8c24f829df355274a94f07cdb43f8cf814e549
checkiamsiam <issiam02415@gmail.com>
2023-01-15 10:53:21 +0600

    design check

--- components/modals/ViewDocumentsModal.js
+++ components/modals/ViewDocumentsModal.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Modal, Text } from "@nextui-org/react";
 import { useSelector } from "react-redux";
 import { AiFillFileText } from "react-icons/ai";
+import { FaDownload } from "react-icons/fa";
 
 export default function ViewDocuments({ visibleDocuments, setVisibleDocuments }) {
   const { installmentDataForModal } = useSelector((state) => state.property);
@@ -17,8 +18,15 @@ export default function ViewDocuments({ visibleDocuments, setVisibleDocuments })
           </Text>
         </Modal.Header>
         <Modal.Body>
-          <div className="h85 w-100 d-flex justify-content-center align-items-center">
-            <AiFillFileText style={{ color: "#FF5A5F" }} />
+          <div style={{ backgroundColor: "#E9EBEC" }}>
+            <div className="h85 w-100 d-flex justify-content-center align-items-center">
+              <AiFillFileText style={{ color: "#FF5A5F", fontSize: "40px" }} />
+            </div>
+            <div className="d-flex justify-content-right">
+              <a href="https://dreamwayapi.sajidurapp.xyz/" className="p-1" style={{ backgroundColor: "white" }}>
+                <FaDownload style={{ color: "#FF5A5F" }} />
+              </a>
+            </div>
           </div>
         </Modal.Body>
       </Modal>
