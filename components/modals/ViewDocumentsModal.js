d4a8479e4f4e486fdf3c69baaed23899997ff5dd
checkiamsiam <issiam02415@gmail.com>
2023-01-15 10:57:15 +0600

    document finish

--- components/modals/ViewDocumentsModal.js
+++ components/modals/ViewDocumentsModal.js
@@ -18,12 +18,18 @@ export default function ViewDocuments({ visibleDocuments, setVisibleDocuments })
           </Text>
         </Modal.Header>
         <Modal.Body>
-          <div style={{ backgroundColor: "#E9EBEC" }}>
+          <div style={{ backgroundColor: "#E9EBEC" }} className="py-4">
             <div className="h85 w-100 d-flex justify-content-center align-items-center">
               <AiFillFileText style={{ color: "#FF5A5F", fontSize: "40px" }} />
             </div>
             <div className="d-flex justify-content-right">
-              <a href="https://dreamwayapi.sajidurapp.xyz/" className="p-1" style={{ backgroundColor: "white" }}>
+              <a
+                href="https://dreamwayapi.sajidurapp.xyz/"
+                rel="noreferrer"
+                target="_blank"
+                className="p-2 rounded-lg"
+                style={{ backgroundColor: "white" }}
+              >
                 <FaDownload style={{ color: "#FF5A5F" }} />
               </a>
             </div>
