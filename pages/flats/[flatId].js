7838185918731667ed569ebf73d94aedbd16b464
checkiamsiam <issiam02415@gmail.com>
2023-01-17 01:08:12 +0600

    rearrange

--- pages/flats/[flatId].js
+++ pages/flats/[flatId].js
@@ -1,17 +1,11 @@
-// import dynamic from "next/dynamic";
-import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
 import Seo from "../../components/common/seo";
 import DetailContent from "../../components/flats/Flatdetails/DetailContent";
 import FlatDetailBannerGellary from "../../components/flats/Flatdetails/FlatDetailBannerGellary";
-import { useGetFlatDetailQuery } from "../../features/flat/flatApi";
-// import axiosApi from "../../features/axiosInstance";
+import axiosApi from "../../features/axiosInstance";
 
-function FlatDetails() {
-  const router = useRouter();
-  const { isLoading, data, isError } = useGetFlatDetailQuery(router?.query?.flatId);
-  const flat = data?.response[0];
-  console.log(flat);
 
+function FlatDetails({ flat }) {
   return (
     <>
       <Seo pageTitle="Flat Details" />
@@ -23,12 +17,12 @@ function FlatDetails() {
 
 export default FlatDetails;
 
-// export async function getServerSideProps(cxt) {
-//   const id = cxt.query.flatId;
-//   const { data } = await axiosApi.get(`/api/flat/${id}`);
-//   return {
-//     props: {
-//       flat: data?.response[0],
-//     },
-//   };
-// }
+export async function getServerSideProps(cxt) {
+  const id = cxt.query.flatId;
+  const { data } = await axiosApi.get(`/api/flat/${id}`);
+  return {
+    props: {
+      flat: data?.response[0],
+    },
+  };
+}
