1143442995d40aee7fbaa4ae03bb1de319c3dd95
checkiamsiam <issiam02415@gmail.com>
2023-01-16 23:20:16 +0600

    test

--- components/flats/Flatdetails/flatLocationMap.js
+++ components/flats/Flatdetails/flatLocationMap.js
@@ -1,3 +1,4 @@
+"use client";
 import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 
 function FlatLocationMap({ flat }) {
@@ -5,17 +6,17 @@ function FlatLocationMap({ flat }) {
   console.log(flat);
   return (
     <>
-      {flat?.lt && flat?.ln && (
-        <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={position}>
-            <Popup>{flat?.address}</Popup>
-          </Marker>
-        </MapContainer>
-      )}
+      (
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>{flat?.address}</Popup>
+        </Marker>
+      </MapContainer>
+      )
     </>
   );
 }
