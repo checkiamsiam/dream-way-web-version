cde952964f8ad786accce7f33eddec4a578ee6db
checkiamsiam <issiam02415@gmail.com>
2023-01-18 00:31:36 +0600

    build

--- components/common/locationMap.js
+++ components/common/locationMap.js
@@ -1,23 +1,7 @@
-import { MapContainer, Marker, TileLayer } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import { useSelector } from "react-redux";
+// import toHtml from 'string-to-html';
 
-function LocationMap() {
-  const { locationPageData } = useSelector((state) => state.property);
-  const position = [90.4138067, 23.8110257];
-  return (
-    <>
-      {/* {locationPageData?.lt && locationPageData?.ln && ( */}
-      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}></Marker>
-      </MapContainer>
-      {/* )} */}
-    </>
-  );
-}
+// function LocationMap({ mapString }) {
+//   return <>{toHtml(mapString)}</>;
+// }
 
-export default LocationMap;
+// export default LocationMap;
