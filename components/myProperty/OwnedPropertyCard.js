6b7928cc55bb83c8790bc07f71e6c21cae65c46e
checkiamsiam <issiam02415@gmail.com>
2023-01-18 10:21:55 +0600

    camera roll button

--- components/myProperty/OwnedPropertyCard.js
+++ components/myProperty/OwnedPropertyCard.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { AiFillFileText } from "react-icons/ai";
 import { RiInstallLine } from "react-icons/ri";
 import { FaMapMarkedAlt } from "react-icons/fa";
+import { GoDeviceCamera } from "react-icons/go";
 import { useDispatch } from "react-redux";
 import { setInstallmentProperty, setLocationPageData } from "../../features/property/propertySlice";
 import { convertStringToArray } from "../common/utilityFunctions";
@@ -32,51 +33,67 @@ function OwnedPropertyCard({ property, setVisibleModal, setVisibleDocuments }) {
     router.push("/property-location");
   };
   return (
-    <div className="feat_property list favorite_page">
-      <div className="thumb">
-        <Image
-          className="img-whp cover w-auto h-auto"
-          src={`https://dreamwayapi.sajidurapp.xyz/${convertStringToArray(property?.property?.img_url)[0]}`}
-          alt="fp1.jpg"
-          width={360}
-          height={240}
-        />
-      </div>
-
-      <div className="details">
-        <div className="tc_content">
-          <h4>
-            {" "}
-            <Link href="">{property?.property?.title}</Link>
-          </h4>
-          <p>
-            <span className="flaticon-placeholder"></span> {property?.property?.address}
-          </p>
-          <a className="fp_price text-thm" href="#">
-            {property?.property?.price || property?.property?.regular_price} BDT
-          </a>
+    <div>
+      <div className="feat_property list favorite_page">
+        <div className="thumb">
+          <Image
+            className="img-whp cover w-auto h-auto"
+            src={`https://dreamwayapi.sajidurapp.xyz/${convertStringToArray(property?.property?.img_url)[0]}`}
+            alt="fp1.jpg"
+            width={360}
+            height={240}
+          />
         </div>
-      </div>
-      {/* End details */}
 
-      <ul className="view_edit_delete_list mb0 mt35">
-        <li onClick={handleOpenLocationMap} className="list-inline-item" data-toggle="tooltip" data-placement="top" title="See On Map">
-          <a href="#">
-            <FaMapMarkedAlt style={{ color: "#FF5A5F" }} />
-          </a>
-        </li>
-        <li onClick={handleOpenInstallmentModal} className="list-inline-item" data-toggle="tooltip" data-placement="top" title="Installment">
-          <a href="#">
-            <RiInstallLine style={{ color: "#FF5A5F" }} />
-          </a>
-        </li>
-        <li onClick={handleOpenDocumentsModal} className="list-inline-item" data-toggle="tooltip" data-placement="top" title="Documents">
-          <a href="#">
-            <AiFillFileText style={{ color: "#FF5A5F" }} />
-          </a>
-        </li>
-      </ul>
-      {/* view_edit_delete_list */}
+        <div className="details">
+          <div className="tc_content">
+            <h4>
+              {" "}
+              <Link href="">{property?.property?.title}</Link>
+            </h4>
+            <p>
+              <span className="flaticon-placeholder"></span> {property?.property?.address}
+            </p>
+            <a className="fp_price text-thm" href="#">
+              {property?.property?.price || property?.property?.regular_price} BDT
+            </a>
+          </div>
+          <ul className="d-flex justify-content-center gap-2">
+            
+            {[4, 4, 6, 0].map((data, i) => (
+              <li key={i} class="list-inline-item position-relative">
+              <a href="#">
+                <GoDeviceCamera style={{ color: "#FF5A5F" , fontSize: "20px" }} />
+              <span class="position-absolute top-0 start-100 translate-middle badge p-1 bg-danger">
+                {i+1}
+              </span>
+              </a>
+            </li>
+              
+            ))}
+          </ul>
+        </div>
+        {/* End details */}
+
+        <ul className="view_edit_delete_list mb0 mt35">
+          <li onClick={handleOpenLocationMap} className="list-inline-item" data-toggle="tooltip" data-placement="top" title="See On Map">
+            <a href="#">
+              <FaMapMarkedAlt style={{ color: "#FF5A5F" }} />
+            </a>
+          </li>
+          <li onClick={handleOpenInstallmentModal} className="list-inline-item" data-toggle="tooltip" data-placement="top" title="Installment">
+            <a href="#">
+              <RiInstallLine style={{ color: "#FF5A5F" }} />
+            </a>
+          </li>
+          <li onClick={handleOpenDocumentsModal} className="list-inline-item" data-toggle="tooltip" data-placement="top" title="Documents">
+            <a href="#">
+              <AiFillFileText style={{ color: "#FF5A5F" }} />
+            </a>
+          </li>
+        </ul>
+        {/* view_edit_delete_list */}
+      </div>
     </div>
   );
 }
