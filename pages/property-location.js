7ba4b6ae6000a0a9e7b68b5d3944899b109138bd
checkiamsiam <issiam02415@gmail.com>
2023-01-20 09:54:47 +0600

    check

--- pages/property-location.js
+++ pages/property-location.js
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import parse from "html-react-parser";
+// import parse from "html-react-parser";
 import Seo from "../components/common/seo";
 
 function PropertyLocation() {
@@ -18,7 +18,7 @@ function PropertyLocation() {
 
           <div className="col-lg-12">
             <div className="h600" id="map-canvas">
-              <div className="gmap_canvas pe-none">{parse(locationPageData?.iframeLocation)}</div>
+              {/* <div className="gmap_canvas pe-none">{parse(locationPageData?.iframeLocation)}</div> */}
             </div>
           </div>
         </div>
